86c7735ead53e86562bf9bb0b8eb065b58663c81
Dave Shawley <daves@aweber.com>
2015-06-19 10:33:54 -0400

    Add requirement files.

--- setup.py
+++ setup.py
@@ -7,6 +7,22 @@ import setuptools
 import bandoleers
 
 
+def read_requirements(section):
+    requirements = []
+    with open('requires/{0}'.format(section)) as f:
+        for line in f:
+            if '#' in line:
+                line = line[:line.index('#')]
+            line = line.strip()
+            if line.startswith('-i'):
+                continue
+            if line.startswith('-r'):
+                requirements.extend(read_requirements(line[2:].strip()))
+                continue
+            requirements.append(line)
+    return requirements
+
+
 setuptools.setup(
     name='bandoleers',
     description='AWeber development tool belt',
@@ -17,6 +33,8 @@ setuptools.setup(
     url='http://github-enterprise.colo.lair/Platform/bandoleers',
     author='AWeber Communications, Inc.',
     author_email='api@aweber.com',
+    install_requires=read_requirements('installation.txt'),
+    tests_require=read_requirements('testing.txt'),
     classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
