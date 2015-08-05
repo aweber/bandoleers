61d5ffeea2795cf4a0805ff5f3c7af6a86f50fd9
Dave Shawley <daves@aweber.com>
2015-08-05 10:21:56 -0400

    wait-for: Add cassandra URL support.

--- bandoleers/waitfor.py
+++ bandoleers/waitfor.py
@@ -3,31 +3,48 @@
 wait-for -- wait for a URL to become available
 
 Usage:
-    wait-for <URL> [--timeout=seconds]
+    wait-for <URL> [--timeout=seconds] [-v |--verbose]
     wait-for (-h | --help | --version)
 
 """
+import asyncore
 import logging
-import time
+import socket
 import sys
+import time
 try:
     from urllib import parse
-except:
+except ImportError:
     import urlparse as parse
 
+from cassandra import cluster
 import docopt
 import requests.exceptions
 
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.ERROR,
     format='%(relativeCreated)-10d %(levelname)-8s %(message)s')
+LOGGER = logging.getLogger(__name__)
 
 
 def connect_to(url, timeout):
     scheme, netloc, path, query, fragment = parse.urlsplit(url)
     if scheme in ('http', 'https'):
         requests.get(url, timeout=timeout)
+        return True
+
+    elif scheme == 'cassandra':
+        host, _, port = netloc.partition(':')
+        _, _, ip_addrs = socket.gethostbyname_ex(host)
+        conn = cluster.Cluster(contact_points=ip_addrs, port=int(port) or 9042,
+                               control_connection_timeout=timeout)
+        conn.connect()
+        asyncore.close_all()
+        asyncore.loop()
+
+        return True
+
     else:
         raise RuntimeError("I don't know what to do with {0}".format(scheme))
 
@@ -36,6 +53,11 @@ def run():
     logger = logging.getLogger(__name__)
     opts = docopt.docopt(__doc__)
     timeout = opts.get('--timeout', None)
+    sleep_time = 0.25
+
+    if opts.get('--verbose', False) or opts.get('-v', False):
+        logging.getLogger().setLevel(logging.DEBUG)
+    logging.getLogger('cassandra').setLevel(logging.CRITICAL)
 
     t0 = time.time()
     wait_forever = timeout is None
@@ -43,18 +65,10 @@ def run():
     logger.debug('waiting for %s', 'forever' if wait_forever else timeout)
     while wait_forever or (time.time() - t0) < timeout:
         try:
-            try:
-                connect_to(opts['<URL>'], timeout=timeout)
+            if connect_to(opts['<URL>'], timeout=timeout):
                 logger.debug('connection to %s succeeded after %f seconds',
                              opts['<URL>'], time.time() - t0)
                 sys.exit(0)
-
-            except requests.exceptions.Timeout:
-                break
-
-            except requests.exceptions.ConnectionError:
-                logger.debug('connection error occurred, sleeping')
-                time.sleep(0.25)
 
         except RuntimeError:
             logger.exception('internal failure')
@@ -63,6 +77,10 @@ def run():
         except KeyboardInterrupt:
             logger.info('killed')
             sys.exit(-1)
+
+        except Exception as error:
+            logger.debug('%s, sleeping for %f seconds', error, sleep_time)
+            time.sleep(sleep_time)
 
     logger.error('wait timed out after %f seconds', time.time() - t0)
     sys.exit(-1)
